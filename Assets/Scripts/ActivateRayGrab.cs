944d52dd63131354d2f22fd8f919c2d95d12bf8a
Tom Sautron <78366314+SautronT@users.noreply.github.com>
2024-10-15 15:26:24 +0200

    rd

---
+++ Assets/Scripts/ActivateRayGrab.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ActivateRayGrab : MonoBehaviour
+{
+    public GameObject leftGrabRay;
+    public GameObject rightGrabRay;
+    public XRDirectInteractor leftDirectGrab;
+    public XRDirectInteractor rightDirectGrab;
+    void Update()
+    {
+        leftGrabRay.SetActive(leftDirectGrab.interactablesSelected.Count == 0);
+        rightGrabRay.SetActive(rightDirectGrab.interactablesSelected.Count == 0);
+    }
+}
---
+++ Assets/Scripts/ActivateTeleportationRay.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+public class ActivateTeleportationRay : MonoBehaviour
+{
+    public GameObject leftTeleportation;
+    public GameObject rightTeleportation;
+    public InputActionProperty leftActivate;
+    public InputActionProperty rightActivate;
+    public InputActionProperty leftCancel;
+    public InputActionProperty rightCancel;
+    public XRRayInteractor leftRay;
+    public XRRayInteractor rightRay;
+
+
+    void Update()
+    {
+        bool isLeftRayHovering = leftRay.TryGetHitInfo(out Vector3 leftPos, out Vector3 leftNormal, out int leftNumber, out bool leftValid);
+        bool isRightRayHovering = rightRay.TryGetHitInfo(out Vector3 rightPos, out Vector3 rightNormal, out int rightNumber, out bool rightValid);
+
+        leftTeleportation.SetActive(!isLeftRayHovering && leftCancel.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() > 0.1);
+        rightTeleportation.SetActive(!isRightRayHovering && rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1);
+    }
+}
