d5c1cb3090525c0bdd7dfe283940556ac7269f4b
Tom Sautron <78366314+SautronT@users.noreply.github.com>
2024-10-24 14:22:11 +0200

    menu avec teleportation

--- Assets/Scripts/GameMenuManager.cs
+++ Assets/Scripts/GameMenuManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameMenuManager : MonoBehaviour
 {
@@ -9,6 +10,26 @@ public class GameMenuManager : MonoBehaviour
     public GameObject menu;
     public InputActionProperty showButton;
 
+    public Button noviceButton;
+    public Button normalButton;
+    public Button expertButton;
+    public Button trainingButton;
+    public Button restartButton; // Nouveau bouton pour redémarrer le jeu
+
+    public Transform noviceSpawn; // Point de téléportation Novice
+    public Transform normalSpawn; // Point de téléportation Normal
+    public Transform expertSpawn; // Point de téléportation Expert
+    public Transform trainingSpawn; // Point de téléportation Training
+
+    void Start()
+    {
+        noviceButton.onClick.AddListener(TeleportToNovice);
+        normalButton.onClick.AddListener(TeleportToNormal);
+        expertButton.onClick.AddListener(TeleportToExpert);
+        trainingButton.onClick.AddListener(TeleportToTraining);
+        restartButton.onClick.AddListener(ReloadScene); // Lier le bouton de redémarrage
+    }
+
     void Update()
     {
         if (showButton.action.WasPressedThisFrame())
@@ -16,6 +37,7 @@ public class GameMenuManager : MonoBehaviour
             menu.SetActive(!menu.activeSelf);
             menu.transform.position = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * distanceFromHead;
         }
+
         menu.transform.LookAt(new Vector3(head.position.x, menu.transform.position.y, head.position.z));
         menu.transform.forward *= -1;
     }
@@ -23,5 +45,29 @@ public class GameMenuManager : MonoBehaviour
     public void ReloadScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void TeleportToNovice() => TeleportTo(noviceSpawn);
+    public void TeleportToNormal() => TeleportTo(normalSpawn);
+    public void TeleportToExpert() => TeleportTo(expertSpawn);
+    public void TeleportToTraining() => TeleportTo(trainingSpawn);
+
+    private void TeleportTo(Transform spawnPoint)
+    {
+        if (spawnPoint == null)
+        {
+            Debug.LogError("Spawn point is not assigned!");
+            return;
+        }
+
+        GameObject player = GameObject.Find("XR Origin (VR)"); // Vérifie le nom de l'objet joueur
+        if (player != null)
+        {
+            player.transform.position = spawnPoint.position;
+        }
+        else
+        {
+            Debug.LogError("Player object not found!");
+        }
     }
 }
